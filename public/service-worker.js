32a68afc84efba60fbf205340f56f2dbb8a3f3c2
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-02 01:21:27 -0500

    add install event to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,16 @@
 // files to cache
 
 //install event listener to cache files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log("Files were pre-cached successfully!");
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+    self.skipWaiting();
+});
 
 // activate event listener
 
