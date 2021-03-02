e6c0ed4d2d7fc59a6a20ec919d29779e1611437c
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-02 16:57:08 -0500

    make db a global variable, update onsucess to check if the app is online

--- public/db.js
+++ public/db.js
@@ -1,10 +1,17 @@
+let db;
+
 // open the budget database
 const request = indexedDB.open('budget_database', 1);
 
-request.onupgradeneeded(({target}) => {
-    const db = target.result;
+request.onupgradeneeded(({ target }) => {
+    db = target.result;
     const objectStore = db.createObjectStore('transactions');
     objectStore.createIndex('transaction', 'transaction')
 });
 
-request.onsuccess(event => console.log(event.result));
+request.onsuccess(({ target }) => {
+    db = target.result;
+
+    // check the online database if the app is online
+    if (navigator.onLine) checkDatabase();
+});
