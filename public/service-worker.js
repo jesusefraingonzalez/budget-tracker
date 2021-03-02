3f25ac3e2fcaf2592d6e9338de785af3db8000b7
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-02 01:26:36 -0500

    add files to cache, cache names

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,16 @@
 // files to cache
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+];
+
+const CACHE_NAME = 'static-cache-v1';
+const DATA_CACHE = 'data-cache-v1';
 
 //install event listener to cache files
 self.addEventListener('install', (event) => {
